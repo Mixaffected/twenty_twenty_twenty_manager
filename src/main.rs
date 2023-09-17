fa7141774dee46f481b2944d59f9756d9495cf01
Mixaffected <marvin.dziedzina@gmail.com>
2023-09-17 13:01:36 +0200

    Replaced with rust version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use rodio::{source::Source, Decoder, OutputStream};
+use std::{fs::File, io::BufReader, thread, time};
+
+fn sleep(secs: u64) {
+    thread::sleep(time::Duration::from_secs(secs));
+}
+
+fn play_sound() {
+    // create the outputStream object
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    // read the audio file
+    let file = BufReader::new(File::open("audio/reminder.mp3").unwrap());
+    // create the decoded and ready to output audio
+    let source = Decoder::new(file).unwrap().convert_samples();
+
+    // play the audio
+    stream_handle
+        .play_raw(source)
+        .expect("Something went wrong!");
+
+    sleep(3)
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        sleep(1200);
+
+        play_sound();
+
+        sleep(2);
+
+        play_sound();
+    }
 }
